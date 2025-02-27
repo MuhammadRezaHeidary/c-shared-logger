207dc3f46c442aeb3e4579ea67ae0c8eddc61a8f
reza <muhammadreza.heidary@gmail.com>
2025-02-28 00:22:08 +0330

    edit examples

--- example/log_html_file/logFileHtml_ProducerDog.cpp
+++ example/log_html_file/logFileHtml_ProducerDog.cpp
@@ -6,7 +6,7 @@ using namespace mekatronik::logger;
 
 int main() {
     std::string shared_memory_name = "/shm_html_animal";
-    std::string log_topic = "Animal";
+    std::string log_topic = "Animal::Dog";
     std::string log_file_name = "log_animal.html";
     SharedMemoryLogger logger(shared_memory_name, log_topic, log_file_name, SharedMemoryLogger::LogFormat::HTML);
 
--- example/log_text_file/logFileText_ProducerFootball.cpp
+++ example/log_text_file/logFileText_ProducerFootball.cpp
@@ -6,7 +6,7 @@ using namespace mekatronik::logger;
 
 int main() {
     std::string shared_memory_name = "/shm_text_sports";
-    std::string log_topic = "Sports";
+    std::string log_topic = "Sports::Football";
     std::string log_file_name = "log_sports.txt";
     SharedMemoryLogger logger(shared_memory_name, log_topic, log_file_name);
 
